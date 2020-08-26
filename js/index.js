7950c2e43bf96ffa9efd741b2df6c8d7c8bb300e
pmartinsn <pamela.illisse@gmail.com>
2020-08-26 16:26:18 -0300

    function 'onclick' added

--- js/index.js
+++ js/index.js
@@ -32,7 +32,6 @@
             let audio = new Audio("audio/W.mp3");
             audio.play();
             console.log("The 'W' is pressed.");
-
         } else if (event.code==="KeyE") {
             let audio = new Audio("audio/E.mp3");
             audio.play();
@@ -53,9 +52,56 @@
             console.log("wrong key pressed");
         }
     });
-
-
-
+    document.addEventListener("click", function(event) {
+        A.onclick = function (event){
+            let audio = new Audio("audio/A.mp3");
+            audio.play();
+        }
+        S.onclick = function (event){
+            let audio = new Audio("audio/S.mp3");
+            audio.play();
+        }
+        D.onclick = function (event){
+            let audio = new Audio("audio/D.mp3");
+            audio.play();
+        }
+        F.onclick = function (event){
+            let audio = new Audio("audio/F.mp3");
+            audio.play();
+        }
+        G.onclick = function (event){
+            let audio = new Audio("audio/G.mp3");
+            audio.play();
+        }
+        H.onclick = function (event){
+            let audio = new Audio("audio/H.mp3");
+            audio.play();
+        }
+        J.onclick = function (event){
+            let audio = new Audio("audio/J.mp3");
+            audio.play();
+        }
+        W.onclick = function (event){
+            let audio = new Audio("audio/W.mp3");
+            audio.play();
+        }
+        E.onclick = function (event){
+            let audio = new Audio("audio/E.mp3");
+            audio.play();
+        }
+        T.onclick = function (event){
+            let audio = new Audio("audio/T.mp3");
+            audio.play();
+        }
+        Y.onclick = function (event){
+            let audio = new Audio("audio/Y.mp3");
+            audio.play();
+        }
+        U.onclick = function (event){
+            let audio = new Audio("audio/U.mp3");
+            audio.play();
+        }
+    });
 
 
 
